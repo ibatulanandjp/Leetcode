d8e21a414acab467fcd06e2b7195d4ebcfeaf1dc
unknown <atul.anand@tscawspj1.onmicrosoft.com>
2022-06-14 22:48:24 +0900

    Updated folder structure

---
+++ #1_TwoSum/solution.py
@@ -0,0 +1,11 @@
+# NOTE: Commented codes are for local testing purpose only
+# ---------------------------------------------------------
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        print("Hello")
+
+sol = Solution()
+result = sol.twoSum([2,7,11,15], 9)

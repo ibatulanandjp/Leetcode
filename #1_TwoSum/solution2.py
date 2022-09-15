979e423ad1dff80c9a2632fa40e62c743e7e3845
unknown <atul.anand@tscawspj1.onmicrosoft.com>
2022-09-16 02:49:45 +0900

    Updated #1 sol2

--- #1_TwoSum/solution2.py
+++ #1_TwoSum/solution2.py
@@ -3,13 +3,13 @@ from typing import List
 class Solution:
     def twoSum(self, nums:List[int], target: int) -> List[int] :
         seenMap={}
-        for i, val in enumerate(nums) :
+        for i in range(len(nums)) :
             remaining = target - nums[i]
 
             if remaining in seenMap : 
                 return [seenMap[remaining], i]
 
-            seenMap[val] = i
+            seenMap[nums[i]] = i
 
 sol = Solution()
 result = sol.twoSum([2,7,11,15], 9)

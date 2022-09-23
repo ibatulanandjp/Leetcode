875455d49a009de4026ecd24a4110c685643b5ed
Atul Anand <ib.atulanandjp@gmail.com>
2022-09-24 00:15:33 +0900

    Added solution2 for #116

---
+++ #116_PopulatingNextRightPointersInEachNode/solution2.py
@@ -0,0 +1,35 @@
+
+from collections import deque
+from typing import Optional
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+
+        if not root:
+            return None
+        queue = deque([root])
+
+        while queue:
+
+            node = queue.popleft()
+
+            if node.left and node.right:
+
+                node.left.next = node.right
+                if node.next:
+                    node.right.next = node.next.left
+
+                queue.append(node.left)
+                queue.append(node.right)
+
+        return root

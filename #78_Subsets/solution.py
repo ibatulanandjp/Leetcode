98f59a679f1cb011b00aa2cc83005f51edc4b9ca
unknown <atul.anand@tscawspj1.onmicrosoft.com>
2022-09-25 12:30:22 +0900

    Added #78

---
+++ #78_Subsets/solution.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        powerset, stack = [[]], [[]]
+
+        for n in nums:
+            stack.append([n])
+            powerset.append([n])
+
+        while stack:
+            curr_set = stack.pop()
+
+            for n in nums:
+                if n not in curr_set:
+                    temp = curr_set[:]  # Deep Copy
+                    temp.append(n)
+
+                    # Sort the array to avoid duplicates
+                    temp = sorted(temp)
+                    if temp not in powerset:
+                        stack.append(temp)
+                        powerset.append(temp)
+
+        return powerset

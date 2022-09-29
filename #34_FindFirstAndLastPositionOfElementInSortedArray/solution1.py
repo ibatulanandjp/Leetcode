c0f890422bed1fbd277d742da1dbbc42ca226f87
unknown <atul.anand@tscawspj1.onmicrosoft.com>
2022-09-29 20:18:45 +0900

    Added #34

---
+++ #34_FindFirstAndLastPositionOfElementInSortedArray/solution1.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+
+        res = [-1, -1]
+
+        for i in range(len(nums)):
+            if nums[i] == target:
+                if res[0] == -1:
+                    res[0] = i
+                    res[1] = i
+
+                else:
+                    res[1] = i
+
+        return res
---
+++ #34_FindFirstAndLastPositionOfElementInSortedArray/solution2.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        return[self.firstIndex(nums, target), self.lastIndex(nums, target)]
+    
+    def firstIndex(self, nums: List[int], target: int) -> int:
+        index=-1
+        low = 0
+        high = len(nums)-1
+        
+        while low<=high:
+            mid = math.floor(low + (high-low)/2)
+            if nums[mid] == target:
+                index = mid
+                high = mid-1
+                
+            elif nums[mid] > target:
+                high = mid-1
+                
+            else:
+                low = mid+1
+                
+        return index
+    
+    def lastIndex(self, nums: List[int], target: int) -> int:
+        index=-1
+        low = 0
+        high = len(nums)-1
+        
+        while low<=high:
+            mid = math.floor(low + (high-low)/2)
+            if nums[mid] == target:
+                index = mid
+                low = mid+1
+                
+            elif nums[mid] > target:
+                high = mid-1
+                
+            else:
+                low = mid+1
+                
+        return index

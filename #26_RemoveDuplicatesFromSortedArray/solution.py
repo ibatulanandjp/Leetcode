92d716eeb40a07ceab0f5e5265785ef60f01dd66
Atul Anand <atul.anand@tscawspj1.onmicrosoft.com>
2023-04-22 22:56:04 +0900

    Added comment to #26

--- #26_RemoveDuplicatesFromSortedArray/solution.py
+++ #26_RemoveDuplicatesFromSortedArray/solution.py
@@ -3,9 +3,14 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        x = 1
+        # Variable to count of unique elements in the list
+        unique_count = 1
+
+        # For every element in the list
         for i in range(len(nums)-1):
             if nums[i] != nums[i+1]:
-                nums[x] = nums[i+1]
-                x += 1
-        return x
+                # Replace the element at unique_count with the new unique element found in the list
+                nums[unique_count] = nums[i+1]
+                # Increment the unique_count
+                unique_count += 1
+        return unique_count

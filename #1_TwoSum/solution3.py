ce38f2a6ab6d061f4da767d843f81d156e0f4073
Atul Anand <atul.anand@tscawspj1.onmicrosoft.com>
2023-04-24 01:07:44 +0900

    Added solution3 to #1

---
+++ #1_TwoSum/solution3.py
@@ -0,0 +1,19 @@
+# Different version of solution2
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Map to store remaining number
+        seen_map = {}
+
+        for i, n in enumerate(nums):
+            # Remaining number to find
+            remaining = target - n
+
+            # If remaining number was seen already, return the indices
+            if remaining in seen_map:
+                return [seen_map.get(remaining), i]
+
+            # (Else) Add the index to the seen map
+            seen_map[n] = i

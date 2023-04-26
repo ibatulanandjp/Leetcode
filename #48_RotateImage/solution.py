599bedf6809034805c7b1af6eac2028e11ce604b
Atul Anand <atul.anand@tscawspj1.onmicrosoft.com>
2023-04-27 01:24:20 +0900

    Updated import package

--- #48_RotateImage/solution.py
+++ #48_RotateImage/solution.py
@@ -1,17 +1,20 @@
+from typing import List
+
+
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
-        """        
+        """
         # Step1: Reverse the matrix
         matrix[:] = matrix[::-1]
-        
+
         n = len(matrix)
         for i in range(n):
             for j in range(n):
-                
+
                 # Step2: Transpose elements
                 # Swap the elements, only for the half of the matrix
                 # Doing it for all will swap twice, so we avoid that
-                if i<j:
+                if i < j:
                     matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]

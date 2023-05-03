b0381e3fd12e221bc3cdc733fff29dacc0f5c02c
Atul Anand <ib.atulanandjp@gmail.com>
2023-05-04 01:02:07 +0900

    Updated comments for #105

--- #105_ConstructBinaryTreeFromPreorderAndInorderTraversal/solution.py
+++ #105_ConstructBinaryTreeFromPreorderAndInorderTraversal/solution.py
@@ -1,8 +1,7 @@
 from typing import List, Optional
 
+
 # Definition for a binary tree node.
-
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -10,6 +9,8 @@ class TreeNode:
         self.right = right
 
 
+# Method: Recursive
+# Get "Root" from preorder, and get "left" and "right" subtree from inorder
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
 
@@ -17,8 +18,10 @@ class Solution:
             return None
 
         root = TreeNode(preorder[0])
+        # Get the middle index from the inorder list
         mid = inorder.index(preorder[0])
 
+        # Recursively call for root.left and root.right
         root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
         root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
         return root

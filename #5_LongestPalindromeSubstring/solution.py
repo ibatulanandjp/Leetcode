34a3e28f9b5e832d426a13f09eabefd7082cc789
Atul Anand <ib.atulanandjp@gmail.com>
2023-05-03 16:31:48 +0900

    Updated code and comments

--- #5_LongestPalindromeSubstring/solution.py
+++ #5_LongestPalindromeSubstring/solution.py
@@ -1,27 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
         res = ""
-        
+
         for i in range(len(s)):
-            # Even case
-            s1 = self.expandAroundCenter(s, i, i)
+            # Odd length palindrome
+            l, r = i, i
+            s1 = self.expandAroundCenter(s, l, r)
             if len(s1) > len(res):
                 res = s1
-            
-            # Odd case
-            s2 = self.expandAroundCenter(s, i, i+1)            
+
+            # Even length palindrome
+            l, r = i, i+1
+            s2 = self.expandAroundCenter(s, l, r)
             if len(s2) > len(res):
                 res = s2
-                   
+
         return res
-    
+
     def expandAroundCenter(self, s: str, left: int, right: int) -> int:
-        if s=="" or left>right: return 0
-        
-        while left>=0 and right<len(s) and s[left]==s[right]:
-            left-=1
-            right+=1
-            
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+
+        # Return left+1 since left is decremented in the last step,
+        # And right remains as is since it was already incremented and acts as the right bound (excluded)
         return s[left+1:right]
-        

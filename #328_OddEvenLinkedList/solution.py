b208df95155152d9f3394d38e5d9d62f47db35aa
Atul Anand <atul.anand@tscawspj1.onmicrosoft.com>
2023-05-03 22:37:59 +0900

    Updated solution for #328

--- #328_OddEvenLinkedList/solution.py
+++ #328_OddEvenLinkedList/solution.py
@@ -12,15 +12,16 @@ class ListNode:
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
+        # Return if only 1 or 2 elements in the list
         if head == None or head.next == None:
             return head
 
         odd = head
         even = head.next
+        # Keep a pointer at the start of even, for adding it to the end of oddList
         evenStart = even
 
-        while even != None and even.next != None:
-
+        while odd.next != None and even.next != None:
             # Process odd nodes
             odd.next = even.next
             odd = odd.next
@@ -28,11 +29,6 @@ class Solution:
             # Process even nodes
             even.next = odd.next
             even = even.next
-
-            # if odd:
-            #     print("Odd", odd.val)
-            # if even:
-            #     print("Even", even.val)
 
         odd.next = evenStart
 

65e121f7e543191eb8b71736c422eeab0c9b5bc5
Atul Anand <ib.atulanandjp@gmail.com>
2023-05-05 00:52:35 +0900

    Updated solution for #62

--- #62_UniquePaths/solution.py
+++ #62_UniquePaths/solution.py
@@ -1,6 +1,11 @@
+# Method: Dynamic Programming
+# Store unique paths from last row and col, i.e. "1" in matrix
+# Go on adding from last to the first cell
+# Return the value at (0,0)
+# TC: O(m*n)
+# SC: O(m*n)
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-
         matrix = []
 
         # Initialize the matrix with
@@ -14,10 +19,9 @@ class Solution:
                 else:
                     row.append(0)
             matrix.append(row)
-        # print(matrix)
 
+        # Traverse every row and col from the end (in opposite direction), and go on adding the values
         r = m-1
-
         while r >= 0:
             c = n-1
             while c >= 0:

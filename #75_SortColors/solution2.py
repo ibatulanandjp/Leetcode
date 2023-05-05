60a119d9ecb6b296a7b5475a043094c4f6547659
Atul Anand <ib.atulanandjp@gmail.com>
2023-05-05 12:45:33 +0900

    Added comments to #75

--- #75_SortColors/solution2.py
+++ #75_SortColors/solution2.py
@@ -11,18 +11,18 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         red, white, blue = 0, 0, len(nums)-1
-        
+
         while white <= blue:
-            
+
             if nums[white] == 0:
                 # Exchange nums[red] and nums[white]
                 nums[red], nums[white] = nums[white], nums[red]
                 red += 1
                 white += 1
-                
+
             elif nums[white] == 1:
                 white += 1
-                
+
             else:
                 # Exchange nums[blue] and nums[white]
                 nums[blue], nums[white] = nums[white], nums[blue]

5856d58faba5a8871b4ef0e001201cde9accd678
Atul Anand <atul.anand@tscawspj1.onmicrosoft.com>
2023-05-22 23:21:48 +0900

    Added solution to #215

--- #215_KthLargestElementInAnArray/solution2.py
+++ #215_KthLargestElementInAnArray/solution2.py
@@ -1,3 +1,15 @@
-'''
-TODO: Using Quick find method (A method similar to Quick sort)
-'''
+# Method: Heap
+import heapq
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        maxHeap = [-n for n in nums]
+        heapq.heapify(maxHeap)
+
+        while k > 1:
+            heapq.heappop(maxHeap)
+            k -= 1
+
+        return -maxHeap[0]

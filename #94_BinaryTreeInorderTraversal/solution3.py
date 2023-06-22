f3ae95568ff85deb7458fb880dc6f6ca96f10eed
Atul Anand <ib.atulanandjp@gmail.com>
2023-06-22 10:12:00 +0900

    Added solution3 for #94

---
+++ #94_BinaryTreeInorderTraversal/solution3.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Inorder: left-Root-right (l-R-r)
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res, stack = [], []
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                node = stack.pop()
+                res.append(node.val)
+                root = node.right
+        return res

3378878c57bf4164709f2ff969b52750415d0fa6
Atul Anand <ib.atulanandjp@gmail.com>
2023-07-28 19:52:11 +0900

    Added solution to #88

--- #88_MergeSortedArray/solution2.py
+++ #88_MergeSortedArray/solution2.py
@@ -4,6 +4,7 @@ class Solution :
     def mergeSortedArray(self, nums1: List[int], m: int, nums2: List[int], n: int) :
         i, j, k = m-1, n-1, m+n-1
 
+        # Go in reverse order and keep on adding the largest num from the 2 lists at the end of nums1 list
         while i>=0 and j>=0 :
             if nums1[i] > nums2[j] :
                 nums1[k] = nums1[i]
@@ -14,6 +15,7 @@ class Solution :
                 j -= 1
                 k -= 1
         
+        # Add the remaining num from the nums2 list into nums1 list 
         while j>=0 :
             nums1[k] = nums2[j]
             j -= 1

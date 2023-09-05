8e7673f7e0a2ea093846ef3beb95cf6f538e6096
Atul Anand <ib.atulanandjp@gmail.com>
2023-09-05 16:53:09 +0900

    Added 138

---
+++ #138_CopyListWithRandomPointer/solution1.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        node_map = {}
+        curr = head
+        while curr:
+            node_map[curr] = Node(curr.val)
+            curr = curr.next
+
+        curr = head
+        while curr:
+            if curr.next:
+                node_map[curr].next = node_map[curr.next]
+            if curr.random:
+                node_map[curr].random = node_map[curr.random]
+            curr = curr.next
+
+        return node_map[head]

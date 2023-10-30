5c6ef7d1dbd085815fdb9748ef2b1d7b6a97fed2
Atul Anand <ib.atulanandjp@gmail.com>
2023-10-30 23:07:14 +0900

    Added #1356

---
+++ #1356_SortIntegersByTheNumberOf1Bits/solution1.py
@@ -0,0 +1,10 @@
+# Method: Sort by using Custom Comparator
+# TC: O(n*logn)
+# SC: O(n)
+from typing import List
+
+
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        arr.sort(key=lambda num: (num.bit_count(), num))
+        return arr

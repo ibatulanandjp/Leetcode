9b414cc014e3e77720f0f3154069ea03966e31fb
Atul Anand <ib.atulanandjp@gmail.com>
2023-11-05 17:40:38 +0900

    Added #152

---
+++ #152_MaximumProductSubarray/solution1.py
@@ -0,0 +1,22 @@
+# Method: [Kadane's Algorithm] Calculate curr_max and curr_min, and update max_prod if curr_max is greater than it.
+# Important factor is to consider that -ve value can also yield a maximum product, if another -ve value is found.
+# TC: O(n)
+# SC: O(1)
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        curr_min, curr_max = nums[0], nums[0]
+        max_prod = nums[0]
+        
+        for num in nums[1:]:
+            temp_max = curr_max
+            # Calculate curr_max and curr_min
+            curr_max = max(curr_max*num, curr_min*num, num)
+            curr_min = min(temp_max*num, curr_min*num, num)
+
+            # Calculate max_prod
+            max_prod = max(max_prod, curr_max)
+
+        return max_prod

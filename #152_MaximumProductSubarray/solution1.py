c5b593ca65574daa7831b9cf3fb5dd9b000623f3
Atul Anand <ib.atulanandjp@gmail.com>
2023-11-05 17:43:54 +0900

    Updated solution1 for #152

--- #152_MaximumProductSubarray/solution1.py
+++ #152_MaximumProductSubarray/solution1.py
@@ -13,8 +13,8 @@ class Solution:
         for num in nums[1:]:
             temp_max = curr_max
             # Calculate curr_max and curr_min
-            curr_max = max(curr_max*num, curr_min*num, num)
-            curr_min = min(temp_max*num, curr_min*num, num)
+            curr_max = max(num, curr_max*num, curr_min*num)
+            curr_min = min(num, temp_max*num, curr_min*num)
 
             # Calculate max_prod
             max_prod = max(max_prod, curr_max)

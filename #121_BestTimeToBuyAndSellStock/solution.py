2b185840c1cab05fe8e7245283ec668b55119460
Atul Anand <ib.atulanandjp@gmail.com>
2023-11-05 15:27:00 +0900

    Updated solution1 for #121

--- #121_BestTimeToBuyAndSellStock/solution.py
+++ #121_BestTimeToBuyAndSellStock/solution.py
@@ -1,28 +1,19 @@
+# Method: Use 2 pointers to calculate the max difference
+# TC: O(n)
+# SC: O(1)
 from typing import List
 
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxDiff = 0
-        left = 0
-        right = 1
-
+        max_diff = 0
+        left, right = 0, 1
+        
         while right < len(prices):
             diff = prices[right] - prices[left]
-            # print(diff)
             if diff < 0:
                 left = right
-                right = right+1
-
-            else:
-                if maxDiff < diff:
-                    maxDiff = diff
-                right = right+1
-
-        return maxDiff
-
-
-sol = Solution()
-res = sol.maxProfit([7, 1, 5, 3, 6, 4]) #5
-# res = sol.maxProfit([7, 6, 4, 3, 1]) #0
-print("Max:", res)
+            elif max_diff < diff:
+                max_diff = diff
+            right += 1
+        return max_diff

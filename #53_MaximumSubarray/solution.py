876a69bd184286faae2d48e003772b54483f1a34
Atul Anand <ib.atulanandjp@gmail.com>
2023-11-05 16:44:26 +0900

    Updated solution1 for #53

--- #53_MaximumSubarray/solution.py
+++ #53_MaximumSubarray/solution.py
@@ -1,31 +1,16 @@
+# Method: [Kadane's Algorithm] Calculate curr_max by adding new num, and update max_sum if curr_max is greater than it.
+# TC: O(n), since traversing the list only once
+# SC: O(1), since no extra space is used
 from typing import List
 
 
 class Solution:
-    def maximumSubarray(self, nums: List[int]) -> int:
-        maxSumSoFar = nums[0]
-        currentMax = 0
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        curr_max = nums[0]
 
-        for i in range(0, len(nums)):
+        for num in nums[1:]:    
+            curr_max = max(num, curr_max + num)
+            max_sum = max(max_sum, curr_max)
 
-            currentMax = currentMax + nums[i]
-
-            # print("Current Max: ", currentMax)
-            # print("MaxSumSoFar: ", maxSumSoFar)
-
-            if maxSumSoFar < currentMax :
-                maxSumSoFar = currentMax
-
-            if currentMax < 0:
-                currentMax = 0
-
-        return maxSumSoFar
-
-
-sol = Solution()
-print(sol.maximumSubarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))    #6
-# print(sol.maximumSubarray([5, 4, -1, 7, 8]))    #23
-# print(sol.maximumSubarray([-2,1]))  #1
-# print(sol.maximumSubarray([-1]))    #-1
-# print(sol.maximumSubarray([-2, -1]))    #-1
-# print(sol.maximumSubarray([-3,-2,0,-1]))    #0
+        return max_sum                

2f4ec8b30d23b4818aace0ed8897889e2a7ff4ad
Atul Anand <ib.atulanandjp@gmail.com>
2023-11-05 15:32:25 +0900

    Updated and added solutions to #121

---
+++ #121_BestTimeToBuyAndSellStock/solution2.py
@@ -0,0 +1,16 @@
+# Method: Calculate max_profit and min_price at each index 
+# TC: O(n)
+# SC: O(1)
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = prices[0]
+
+        for i in range(1, len(prices)):
+            max_profit = max(max_profit, prices[i] - min_price)
+            min_price = min(min_price, prices[i])
+        
+        return max_profit

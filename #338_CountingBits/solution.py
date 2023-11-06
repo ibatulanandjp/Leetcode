eaafc397074cc4ac843bd2c01fc95cf4f7703119
Atul Anand <ib.atulanandjp@gmail.com>
2023-11-06 14:37:56 +0900

    Updated solution1 of #338

--- #338_CountingBits/solution.py
+++ #338_CountingBits/solution.py
@@ -8,5 +8,7 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         res = [0] * (n + 1)
         for i in range(1, n + 1):
+            # i >> 1 is i//2 and it should find the half of i, which has similar no. of 1's (DP way to lookup already calculated value)
+            # i & 1 is i%2 and it checks the last bit 
             res[i] = res[i >> 1] + (i & 1)
         return res

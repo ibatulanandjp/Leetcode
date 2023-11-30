6dd1c64b7f6e8eb9b7608f3a6fa9f7ce467a5400
Atul Anand <ib.atulanandjp@gmail.com>
2023-11-30 22:53:04 +0900

    Added #1611

---
+++ #1611_MinimumOneBitOperationsToMakeIntegersZero/solution1.py
@@ -0,0 +1,11 @@
+# Method: Use bit manipulation
+# TC: O(logn)
+# SC: O(n)
+
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        res = 0
+        while n:
+            res = -res - (n ^ (n - 1))
+            n &= n - 1
+        return abs(res)

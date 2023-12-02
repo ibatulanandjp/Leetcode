c3e638e3e88699c2853a9ffcda42767eae9eb82e
Atul Anand <ib.atulanandjp@gmail.com>
2023-12-02 23:22:37 +0900

    Added #1160

---
+++ #1160_FindWordsThatCanBeFormedByCharacters/solution1.py
@@ -0,0 +1,28 @@
+# Method: Use Hashmap to count
+# TC: O(n + m*k)
+# SC: O(1)
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        counts = defaultdict(int)
+        for c in chars:
+            counts[c] += 1
+        
+        ans = 0
+        for word in words:
+            word_count = defaultdict(int)
+            for c in word:
+                word_count[c] += 1
+            
+            good = True
+            for c, freq in word_count.items():
+                if counts[c] < freq:
+                    good = False
+                    break
+            
+            if good:
+                ans += len(word)
+        return ans

ebc8a52e4b689f638720d96930c46a907f052ed9
Atul Anand <ib.atulanandjp@gmail.com>
2024-02-01 23:51:55 +0900

    Added 2966

---
+++ #2966_DivideArrayIntoArraysWithMaxDifference/solution1.java
@@ -0,0 +1,17 @@
+// Method: Sort the array and check for condition within 3 elements at a time
+// TC: O(n)
+// SC: O(n)
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int[][] res = new int[nums.length / 3][3];
+
+        for (int i = 0; i < nums.length; i += 3) {
+            if (nums[i + 2] - nums[i] > k) {
+                return new int[0][0];
+            }
+            res[i / 3] = new int[]{nums[i], nums[i + 1], nums[i + 2]};
+        }
+        return res;
+    }
+}

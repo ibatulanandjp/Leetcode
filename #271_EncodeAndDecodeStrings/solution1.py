98f93a265e76caf2b2826ef83101d3b133c0f49d
Atul Anand <ib.atulanandjp@gmail.com>
2025-06-16 01:32:44 +0900

    feat: added #271

---
+++ #271_EncodeAndDecodeStrings/solution1.py
@@ -0,0 +1,29 @@
+
+# Method:
+# Use - len(str) + '#' + str - for encoding
+# TC: O(n)
+# SC: O(n)
+from typing import List
+
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + '#' + s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s): 
+            j = i
+            while s[j] != '#':
+                j += 1
+            s_len = int(s[i:j])
+            i = j + 1
+            j = i + s_len
+            res.append(s[i:j])
+            i = j
+        return res

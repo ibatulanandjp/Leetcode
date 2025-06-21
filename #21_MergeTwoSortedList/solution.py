01c4a4d71439beaf83609725cc55c009f6dbb51f
Atul Anand <ib.atulanandjp@gmail.com>
2025-06-22 01:03:09 +0900

    feat: updated solution for #21

--- #21_MergeTwoSortedList/solution.py
+++ #21_MergeTwoSortedList/solution.py
@@ -1,66 +1,27 @@
+# Method: Use a dummy node for result head and iterate through both lists
+# taking the smaller node each time.
+# TC: O(n + m) where n and m are the lengths of the two lists
+# SC: O(1) for the result list, O(n + m) for the input lists if we consider the input as part of the space complexity
 from typing import Optional
 
-
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
-    def __str__(self) -> str:
-        return str(self.val)
-
-
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = curr = ListNode()
 
-        current = currentHead = ListNode()
-
-        # Compare and add the smaller node to the list
         while list1 and list2:
-            if list1.val < list2.val:
-                current.next = list1
-                current = list1
+            if list1.val <= list2.val:
+                curr.next = list1
                 list1 = list1.next
             else:
-                current.next = list2
-                current = list2
+                curr.next = list2
                 list2 = list2.next
+            curr = curr.next
 
-        # Add the remaining nodes from either of the lists to the current list
         if list1 or list2:
-            current.next = list1 if list1 else list2
-
-        return currentHead.next
-
-
-# Create 1st linked list with values
-n11 = ListNode(1)
-n12 = ListNode(2)
-n13 = ListNode(4)
-
-n11.next = n12
-n12.next = n13
-
-# print("---List1---")
-# list1.printList()
-
-
-# Create 2nd linked list with values
-n21 = ListNode(1)
-n22 = ListNode(3)
-n23 = ListNode(4)
-
-n21.next = n22
-n22.next = n23
-
-# print("---List2---")
-# list2.printList()
-
-
-
-sol = Solution()
-res = sol.mergeTwoLists(n11, n21)
-print("---Merged List---")
-while res.next is not None:
-    print(res.val)
-    res = res.next
+            curr.next = list1 if list1 else list2
+        return dummy.next

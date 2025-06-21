0f6143fdebbe33795adc5edc346ca95b801bed1c
Atul Anand <ib.atulanandjp@gmail.com>
2025-06-22 00:27:11 +0900

    feat: added solution 2 to #33

---
+++ #33_SearchInRotatedSortedArray/solution2.py
@@ -0,0 +1,27 @@
+# Method: Binary Search in single pass
+# Check both halves with condition to determine which half to search next
+# TC: O(log n), where n is the number of elements in nums
+# SC: O(1)
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if target == nums[m]:
+                return m
+            
+            if nums[l] <= nums[m]:
+                if target > nums[m] or target < nums[l]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            else:
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return -1

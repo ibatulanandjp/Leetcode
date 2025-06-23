c5bed4b7fcdfea3e4d5fe8f40b5fd5d3a428e0a2
Atul Anand <ib.atulanandjp@gmail.com>
2025-06-24 00:08:21 +0900

    feat: added #23

---
+++ #23_MergeKSortedLists/solution1.py
@@ -0,0 +1,45 @@
+# Method: Divide and Conquer - Merge 2 sorted lists at a time
+# then merge the results until one list remains.
+# TC: O(n log k) where n is the total number of nodes across all lists and k is the number of lists.
+# SC: O(k) for the input lists storage.
+from typing import List, Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+        
+        while len(lists) > 1:
+            mergedLists = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeLists(l1, l2))
+            lists = mergedLists
+        return lists[0]
+
+    def mergeLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = tail = ListNode()
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+
+        return dummy.next

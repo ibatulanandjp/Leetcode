42c38305b7d8606bcb104f37773099da283eef10
Atul Anand <ib.atulanandjp@gmail.com>
2025-06-24 00:49:13 +0900

    feat: updated solutions of #226

---
+++ #226_InvertBinaryTree/solution1.py
@@ -0,0 +1,26 @@
+# Method: DFS - Iteratively swapping left and right children using a stack
+# TC: O(n) where n is the number of nodes in the tree
+# SC: O(n) for the stack
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return root

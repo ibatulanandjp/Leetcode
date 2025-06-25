6b744d2ca9d3cff70a5118e614098bc52ccde7f9
Atul Anand <ib.atulanandjp@gmail.com>
2025-06-25 23:33:27 +0900

    fix: updated solution for #235

--- #235_LowestCommonAncestorOfABinarySearchTree/solution.py
+++ #235_LowestCommonAncestorOfABinarySearchTree/solution.py
@@ -1,39 +1,18 @@
-from typing import List, Optional
-
-
+# Method: Iterative for BST to pick which side to traverse
+# Time: O(h) where h is the height of the tree
+# Space: O(1) since we are not using any extra space
 class TreeNode:
     def __init__(self, val=0, left=None, right=None) -> None:
         self.val = val
         self.left = left
         self.right = right
 
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         while root:
-            if p.val < root.val > q.val:
+            if p.val < root.val and q.val < root.val:
                 root = root.left
-            elif p.val > root.val < q.val:
+            elif p.val > root.val and q.val > root.val:
                 root = root.right
             else:
                 return root
-
-n1 = TreeNode(6)
-n2 = TreeNode(2)
-n3 = TreeNode(8)
-n4 = TreeNode(0)
-n5 = TreeNode(4)
-n6 = TreeNode(7)
-n7 = TreeNode(9)
-
-
-n1.left = n2
-n1.right = n3
-n2.left = n4
-n2.right = n5
-n3.left = n6
-n3.right = n7
-
-sol = Solution()
-res = sol.lowestCommonAncestor(n1, n2, n3)
-print(res.val)

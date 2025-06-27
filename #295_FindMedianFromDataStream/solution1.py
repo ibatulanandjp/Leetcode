d3028627d3e6158e52c3779ec781d9c866d9b62f
Atul Anand <ib.atulanandjp@gmail.com>
2025-06-28 01:21:18 +0900

    feat: added #295

---
+++ #295_FindMedianFromDataStream/solution1.py
@@ -0,0 +1,22 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.nums = []      
+
+    def addNum(self, num: int) -> None:
+        self.nums.append(num)
+
+    def findMedian(self) -> float:
+        self.nums.sort()
+        nlen = len(self.nums)
+
+        mid = nlen // 2
+        if nlen % 2 == 0:
+            return (self.nums[mid-1] + self.nums[mid]) / 2
+        return self.nums[mid]
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()

0cc70db8583eaa8b2655d0f628f55ab980cd638c
Atul Anand <ib.atulanandjp@gmail.com>
2025-07-14 00:03:54 +0900

    feat: added #211

---
+++ #211_DesignAddAndSearchWordsDataStructure/solution1.py
@@ -0,0 +1,49 @@
+# Method: Use a Trie (Prefix Tree) to store words, and
+# for wildcard search, use DFS to explore all possible paths in the Trie.
+# TC: O(n), for addWord and search, where n is the length of the word.
+# SC: O(n), for the Trie structure, where n is the total number of characters in all words added.
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            curr = root
+
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in curr.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+            return curr.end_of_word
+
+        return dfs(0, self.root)
+
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)

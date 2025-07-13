2e9a7b60ab3188382119772bf42df5fef44fd944
Atul Anand <ib.atulanandjp@gmail.com>
2025-07-13 23:43:43 +0900

    feat: added #208

---
+++ #208_ImplementTrie(PrefixTree)/solution1.py
@@ -0,0 +1,45 @@
+# Method: Use hashmap to store children nodes and a boolean to mark end of words.
+# TC: O(n), for insert, search, and startsWith, where n is the length of the word or prefix.
+# SC: O(n), for storing the trie structure.
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.end_of_word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
